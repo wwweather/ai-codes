d3f2da8c516ffd2a4c6fa22253784010ae404553
wwweather <o.v.neliubova@gmail.com>
2023-04-26 23:38:36 +0800

    Create one-liner-maths.py

---
+++ one-liner-maths.py
@@ -0,0 +1,9 @@
+# Here's a complex one-liner that performs multiple tasks in one go. 
+# This will perform arithmetic operations on variables in memory, compare them using logical operators like AND OR NOT, 
+# check equality and inequality with another value, convert values to binary and also calculate the length of strings:
+    
+a >= 0 and b := int(input("Please enter value b: ")), c = len(bin(int(input("Please enter another value c: ") if input() else ""))), e && f || g == h and i = j if k != l else m * n
+    
+# This statement does many things at once so It may be hard for others to read and understand your code. 
+# One liners should generally be avoided since they make code harder to understand, debug and maintain. 
+# Keep in mind there are always cases where you might need to use a one-liner but try to keep these to minimum.
